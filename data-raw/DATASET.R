48fc4066571b8620c0a1ae4ccaed0f1894015649
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2024-11-07 09:21:14 +1000

    consolidate DATASET.R more

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -95,6 +95,26 @@ usethis::use_data(pcqol_raw,
   internal = FALSE
 )
 
+# coefficients
+coefs <- readRDS("data/gcmOnDoWbtFmodel.rda") |>
+  dplyr::mutate(
+    item = substr(attribute, 1, 5),
+    level = substr(attribute, 6, length(attribute)),
+    level_num = dplyr::case_when(
+      level == "Always" ~ 4,
+      level == "Often" ~ 3,
+      level == "Sometimes" ~ 2,
+      level == "Never" ~ 1
+    )
+  )
+
+saveRDS(coefs, "data/gcmOnDoWbtFmodelClean.rda")
+usethis::use_data(coefs,
+                  overwrite = TRUE,
+                  internal = TRUE
+)
+
+
 
 # make hex sticker
 p <- readRDS(file = "data-raw/hs_utils.rds") +
