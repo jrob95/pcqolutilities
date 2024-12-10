39a1d5424f1f3cbbadd4ab0184dba37093ddaf77
jrob95 <jack.m.roberts@gmail.com>
2024-12-10 16:20:13 +1000

    update help on shiny ui

--- inst/shiny-apps/pcqol_utilities_app/ui.R
+++ inst/shiny-apps/pcqol_utilities_app/ui.R
@@ -11,17 +11,17 @@ shiny::fluidPage(
         shiny::tags$ul(
           shiny::tags$li("1 = the more frequent/ greater worry/ concern level, e.g., 'All the time'."),
           shiny::tags$li("7 = the less frequent/ lesser worry/ concern level, e.g., 'None of the time'."),
-          shiny::tags$li("Alternatively, labelled data can be uploaded as long as the labels match the ")
+          shiny::tags$li("Alternatively, labelled data can be uploaded as long as the labels match the text in the 2008 Newcombe et al. article. Labels are not case sensitive.")
         )
       ),
 
       shiny::h3("Manual Input"),
       shiny::numericInput("upset", "Upset", value = NA, min = 1, max = 7),
-      shiny::numericInput("scare", "Scare", value = NA, min = 1, max = 7),
+      shiny::numericInput("scare", "Scared", value = NA, min = 1, max = 7),
       shiny::numericInput("overp", "Overprotective", value = NA, min = 1, max = 7),
-      shiny::numericInput("leave", "Leave", value = NA, min = 1, max = 7),
-      shiny::numericInput("leada", "Lead Activity", value = NA, min = 1, max = 7),
-      shiny::numericInput("awake", "Awake", value = NA, min = 1, max = 7),
+      shiny::numericInput("leave", "Leave child with others", value = NA, min = 1, max = 7),
+      shiny::numericInput("leada", "Lead a normal life", value = NA, min = 1, max = 7),
+      shiny::numericInput("awake", "Awakened during the night", value = NA, min = 1, max = 7),
       shiny::actionButton("add_row", "Add Row"),
 
       shiny::actionButton("calculate", "Calculate Utilities"),
